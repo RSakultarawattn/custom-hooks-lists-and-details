d836f0fec589df14ba6d3d5e79faa2af2de9a72d
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-09 16:38:59 -0800

    character by id test passing

--- src/containers/CharactersPage.jsx
+++ src/containers/CharactersPage.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import CharacterList from '../components/characters/CharacterList';
 import Loading from '../components/loading/Loading';
 import { useCharacters } from '../hooks/UseCharacters';
-import { findCharacters } from '../services/rickAndMorty';
+//import { findCharacters } from '../services/rickAndMorty';
 
 const CharactersPage = () => {
   const { loading, characters } = useCharacters();
